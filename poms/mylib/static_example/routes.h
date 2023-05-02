74223a3e974b21c675d4de61cbe833750aec9351
Dmitriy Moskovskikh <dmitry.smosk@ya.ru>
2023-05-02 10:35:41 +0700

    Static library

---
+++ poms/mylib/static_example/routes.h
@@ -0,0 +1,8 @@
+#ifndef ROUTES_LIB 
+    #define ROUTES_LIB
+    #include "mytypes.h"
+
+    void RoutePrint(struct Route *r);
+    void RoutesPrint(struct Routes *rs);
+    void RoutesFree(struct Routes *rs);
+#endif
